fde7e70b97cc9fd49aa0d08cae209b9cf2820a8f
stucodegreen <m.kuznecovas@gmail.com>
2025-12-19 06:43:17 +0200

    refactor swaylock integration to use a dedicated script for locking

--- scripts/start-swayidle.sh
+++ scripts/start-swayidle.sh
@@ -4,9 +4,9 @@ pkill swayidle
 
 # Start swayidle
 swayidle -w \
-  timeout 300 'swaylock --config ~/.config/swaylock/config --screenshot --ignore-empty-password --daemonize --indicator-caps-lock --indicator --clock --timestr "󰥔 %I:%M %p" --datestr "󰃭 %d-%m-%Y" --show-failed-attempts --indicator-idle-visible' \
+  timeout 300 '~/.config/swaylock/lock.sh' \
   timeout 600 'swaymsg "output * power off"' \
   resume 'swaymsg "output * power on"' \
-  before-sleep 'swaylock --config ~/.config/swaylock/config --screenshot --ignore-empty-password --daemonize --indicator-caps-lock --indicator --clock --timestr "󰥔 %I:%M %p" --datestr "󰃭 %d-%m-%Y" --show-failed-attempts --indicator-idle-visible' \
-  lock 'swaylock --config ~/.config/swaylock/config --screenshot --ignore-empty-password --daemonize --indicator-caps-lock --indicator --clock --timestr "󰥔 %I:%M %p" --datestr "󰃭 %d-%m-%Y" --show-failed-attempts --indicator-idle-visible' &
+  before-sleep '~/.config/swaylock/lock.sh' \
+  lock '~/.config/swaylock/lock.sh' &
 
